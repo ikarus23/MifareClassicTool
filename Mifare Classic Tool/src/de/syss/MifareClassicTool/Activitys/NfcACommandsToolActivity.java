7661a28ac44b3dd00fca9b8d6b15b0a05aea87b0
Ikarus <ikarus4ever@web.de>
2013-05-07 14:07:29 +0200

    Added some functionality to transceive raw commands tool.
    Just for testing, not finished yet!

--- Mifare Classic Tool/src/de/syss/MifareClassicTool/Activitys/NfcACommandsToolActivity.java
+++ Mifare Classic Tool/src/de/syss/MifareClassicTool/Activitys/NfcACommandsToolActivity.java
@@ -18,21 +18,135 @@
 
 package de.syss.MifareClassicTool.Activitys;
 
+import java.io.IOException;
+
+import android.annotation.SuppressLint;
+import android.nfc.tech.NfcA;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+import de.syss.MifareClassicTool.Common;
 import de.syss.MifareClassicTool.R;
 
 // TODO: implement & doc.
 public class NfcACommandsToolActivity extends BasicActivity {
 
+    EditText mCommandText;
+    Button mCommandButton;
+    ToggleButton mConnectToggleButton;
+    TextView mLogText;
+    Button mTimeoutButton;
+    /**
+     * If not null, this is a connected RFID-reader for ISO 14443A tags.
+     */
+    NfcA mNfcA;
+
+    private static final String LOG_TAG =
+            NfcACommandsToolActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nfca_commands_tool);
+
+        mCommandText = (EditText) findViewById(
+                R.id.editTextNfcACommandsToolCommand);
+        mCommandButton = (Button) findViewById(R.id.buttonNfcACommandsToolSend);
+        mTimeoutButton = (Button) findViewById(
+                R.id.buttonNfcACommandsToolTimeout);
+        mConnectToggleButton = (ToggleButton) findViewById(
+                R.id.toggleButtonNfcACommandsToolConnect);
+        mLogText = (TextView) findViewById(R.id.textViewNfcACommandsToolLog);
+    }
+
+    /**
+     * Disconnect (close) {@link #mNfcA} from tag.
+     */
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mNfcA != null && mNfcA.isConnected()) {
+            try {
+                mNfcA.close();
+                mNfcA = null;
+            } catch (IOException e) {
+                Log.d(LOG_TAG, "Error while closing NfcA tag");
+            }
+        }
     }
 
     // TODO: implement & doc.
+    @SuppressLint("NewApi")
     public void onSendCommand(View view) {
+        String command = mCommandText.getText().toString();
+        if (command.equals("")) {
+            // Error. There is no command.
+            // TODO: implement error message.
+            return;
+        }
+        // TODO: distinguish API versions.
+        if (command.length() <= mNfcA.getMaxTransceiveLength()) {
+            try {
+                byte[] tagAnswer = mNfcA.transceive(
+                        Common.hexStringToByteArray(command));
+                // TODO: Do not use fixed strings. This is just for testing.
+                mLogText.setText("Command: " + command + "\n" + "Answer: "
+                        + Common.byte2HexString(tagAnswer) + "\n" + mLogText.getText());
+
+            } catch (IOException e) {
+                // Error.
+                // TODO: implement.
+            }
+        }
+    }
+
+    /**
+     * Connect or disconnect {@link #mNfcA}.
+     * @param view The View object that triggered the method
+     * (in this case the connect/disconnect button).
+     */
+    public void onToggleConnect(View view) {
+        if (mConnectToggleButton.isChecked()) {
+            // Connect.
+            if (Common.getTag() == null) {
+                // Error. There is no tag.
+                Toast.makeText(this, R.string.info_no_nfca_tag_found,
+                        Toast.LENGTH_LONG).show();
+                mConnectToggleButton.setChecked(false);
+                return;
+            }
+            mNfcA = NfcA.get(Common.getTag());
+            try {
+                mNfcA.connect();
+                mTimeoutButton.setEnabled(true);
+                mCommandButton.setEnabled(true);
+            } catch (Exception e) {
+                // Error. Maybe there is no tag.
+                Toast.makeText(this, R.string.info_no_nfca_tag_found,
+                        Toast.LENGTH_LONG).show();
+                mNfcA = null;
+                mConnectToggleButton.setChecked(false);
+            }
+        } else {
+            // Disconnect.
+            try {
+                mNfcA.close();
+            } catch (IOException e) {
+                Log.d(LOG_TAG, "Error while closing NfcA tag");
+            }
+            mNfcA = null;
+            mTimeoutButton.setEnabled(false);
+            mCommandButton.setEnabled(false);
+        }
+    }
+
+    // TODO implement & doc.
+    public void onChangeTimeout(View view) {
 
     }
 
