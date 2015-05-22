a7939813045995991a5b72dc30828bc6ec76eec3
ikarus <ikarus4ever@web.de>
2015-05-22 23:31:04 +0200

    Fixed crash issue for HTC One (m7/m8) with Android 5.x.
    Thnaks to "bildin" and many others for helping to find
    a workaround for this. The real issue is still there and
    has to be fixed by HTC.
    
    It may be possible that this patch will not work for all
    combinations of HTC One devic and Android 5.x versions.

--- Mifare Classic Tool/app/src/main/java/de/syss/MifareClassicTool/MCReader.java
+++ Mifare Classic Tool/app/src/main/java/de/syss/MifareClassicTool/MCReader.java
@@ -29,6 +29,10 @@ import android.content.Context;
 import android.nfc.Tag;
 import android.nfc.TagLostException;
 import android.nfc.tech.MifareClassic;
+import android.nfc.tech.NfcA;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Parcel;
 import android.util.Log;
 import android.util.SparseArray;
 import android.widget.Toast;
@@ -62,7 +66,89 @@ public class MCReader {
      * @param tag The tag to operate on.
      */
     private MCReader(Tag tag) {
-        mMFC = MifareClassic.get(tag);
+        MifareClassic tmpMFC;
+        try {
+            tmpMFC = MifareClassic.get(tag);
+        } catch (NullPointerException e) {
+            tmpMFC = MifareClassic.get(patchTag(tag));
+        }
+        mMFC = tmpMFC;
+    }
+
+    /**
+     * Patch the broken Tag object of HTC One (m7/m8) devices with Android 5.x.
+     * "It seems, the reason of this bug is TechExtras of NfcA is null.
+     * However, TechList contains MifareClassic." -- bildin.
+     * This patch will fix this. For more information please refer to
+     * https://github.com/ikarus23/MifareClassicTool/issues/52
+     * This patch was provided by bildin (https://github.com/bildin).
+     * @param tag The broken tag.
+     * @return The fixed tag.
+     */
+    private Tag patchTag(Tag tag)
+    {
+        if (tag == null) {
+            return null;
+        }
+
+        String[] sTechList = tag.getTechList();
+        Parcel oldParcel;
+        Parcel newParcel;
+        oldParcel = Parcel.obtain();
+        tag.writeToParcel(oldParcel, 0);
+        oldParcel.setDataPosition(0);
+
+        int len = oldParcel.readInt();
+        byte[] id = new byte[0];
+        if (len >= 0) {
+            id = new byte[len];
+            oldParcel.readByteArray(id);
+        }
+        int[] oldTechList = new int[oldParcel.readInt()];
+        oldParcel.readIntArray(oldTechList);
+        Bundle[] oldTechExtras = oldParcel.createTypedArray(Bundle.CREATOR);
+        int serviceHandle = oldParcel.readInt();
+        int isMock = oldParcel.readInt();
+        IBinder tagService;
+        if (isMock == 0) {
+            tagService = oldParcel.readStrongBinder();
+        } else {
+            tagService = null;
+        }
+        oldParcel.recycle();
+
+        int nfcaIdx=-1;
+        int mcIdx=-1;
+        for(int idx = 0; idx < sTechList.length; idx++) {
+            if(sTechList[idx].equals(NfcA.class.getName())) {
+                nfcaIdx = idx;
+            } else if(sTechList[idx].equals(MifareClassic.class.getName())) {
+                mcIdx = idx;
+            }
+        }
+
+        if(nfcaIdx>=0&&mcIdx>=0&&oldTechExtras[mcIdx]==null) {
+            oldTechExtras[mcIdx] = oldTechExtras[nfcaIdx];
+        } else {
+            return tag;
+        }
+
+        newParcel = Parcel.obtain();
+        newParcel.writeInt(id.length);
+        newParcel.writeByteArray(id);
+        newParcel.writeInt(oldTechList.length);
+        newParcel.writeIntArray(oldTechList);
+        newParcel.writeTypedArray(oldTechExtras, 0);
+        newParcel.writeInt(serviceHandle);
+        newParcel.writeInt(isMock);
+        if(isMock == 0) {
+            newParcel.writeStrongBinder(tagService);
+        }
+        newParcel.setDataPosition(0);
+        Tag newTag = Tag.CREATOR.createFromParcel(newParcel);
+        newParcel.recycle();
+
+        return newTag;
     }
 
     /**
