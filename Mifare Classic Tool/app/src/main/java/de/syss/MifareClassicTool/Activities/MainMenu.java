c98107caae5152f0a1720659f20bb9f60e4d2f6f
ikarus <ikarus4ever@web.de>
2020-11-25 23:03:28 +0100

    More consistent menu names.

--- Mifare Classic Tool/app/src/main/java/de/syss/MifareClassicTool/Activities/MainMenu.java
+++ Mifare Classic Tool/app/src/main/java/de/syss/MifareClassicTool/Activities/MainMenu.java
@@ -602,7 +602,7 @@ public class MainMenu extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.general_options, menu);
+        getMenuInflater().inflate(R.menu.main_menu_functions, menu);
         return true;
     }
 
